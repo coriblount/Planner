e3aac41662b832c3e91615c6ef9efb609ef0cf01
sir-udenna <udennaboy@gmail.com>
2021-05-07 14:59:05 -0500

    n

--- app/controllers/finance_items_controller.rb
+++ app/controllers/finance_items_controller.rb
@@ -1,2 +1,8 @@
 class FinanceItemsController < ApplicationController
+
+def index 
+    finance_item = FinanceItem.all
+    render json: finance_item
+end 
+
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,9 @@
 class TripsController < ApplicationController
+
+ def index 
+    trips = Trip.all
+    render json: trips
+end 
+
+
 end
